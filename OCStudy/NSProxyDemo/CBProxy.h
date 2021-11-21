a0a9108efca0d40c408e9eaf64ae5ae5c6531989
hicodeboy <2685312867@qq.com>
2021-11-21 11:32:35 +0800

    proxy1

--- OCStudy/NSProxyDemo/CBProxy.h
+++ OCStudy/NSProxyDemo/CBProxy.h
@@ -2,7 +2,7 @@
 //  CBProxy.h
 //  OCStudy
 //
-//  Created by dujia on 2021/11/21.
+//  Created by hicodeboy on 2021/11/21.
 //
 
 #import <Foundation/Foundation.h>
@@ -10,7 +10,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CBProxy : NSProxy
-
++ (instancetype)proxyWithTarget:(id)target;
+@property (weak, nonatomic) id target;
 @end
 
 NS_ASSUME_NONNULL_END
